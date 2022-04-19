d89e644ebfe7fbf778c555ce7b3dab71820dbd70
lilisaputri <lilisaputri1234@gmail.com>
2022-04-19 10:48:54 +0700

    Merge branch 'main' of https://github.com/MaulanaIT/skripsi-batik into lili

---
+++ src/pages/master/akun.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+// Import Library
+import { Link } from 'react-router-dom';
+import { MdAdd } from 'react-icons/md'
+import Select from 'react-select';
+
+// Import CSS
+import bootstrap from '../../../css/bootstrap.module.css';
+import global from '../../../css/global.module.css';
+import style from '../css/master/akun.module.css';
+
+
+export class akun extends Component {
+    render() {
+        return (
+            <>
+                <div className={style.header}>
+                    <p className={style.title}>Akun</p>
+                    <p className={style.pathname}>Master / Akun </p>
+                </div>
+            </>
+        )
+    }
+}
+
+export default akun
