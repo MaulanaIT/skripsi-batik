19f34f01d35fb3e9f8f47b987d874a3b20cca34d
Maulana <maulanait007@gmail.com>
2022-05-04 08:40:18 +0700

    update API

---
+++ api/master-supplier/select.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../config/connection.php';
+
+$query = "SELECT * FROM master_supplier";
+
+$result = $conn->query($query);
+
+$response = [];
+
+if ($result) {
+    $response['status'] = 200;
+    $response['data'] = [];
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $response['data'] = $row;
+        }
+    } else {
+        $response['data'] = [];
+    }
+} else {
+    $response = mysqli_error($conn);
+}
+
+$response = json_encode($response);
+
+require_once '../config/token.php';
+
+$conn->close();
+?>
