e77a036415da7a681494dda35c101f6618f55498
falda2000 <103646681+falda2000@users.noreply.github.com>
2022-08-15 23:11:25 +0700

    update pop up lihat lagi

--- src/pages/transaksi/produksi/detail_pesanan.js
+++ src/pages/transaksi/produksi/detail_pesanan.js
@@ -22,51 +22,51 @@ export class detail_pesanan extends Component {
                         <p className={global.title}>Detail Pesanan</p>
                             <div className='d-flex'>
                                 <div className={`${global.input_group} col-6 px-2`}>
-                                    <p className={global.title}>Tanggal <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Tanggal</p>
                                     <input type="date" id='input-tanggal-pesan' name='input-tanggal-pesan' readOnly={true}/>
                                 </div>
                                 <div className={`${global.input_group} col-6 pe-2`}>
-                                    <p className={global.title}>Kode Pesanan <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Kode Pesanan</p>
                                     <input type="text" id='input-kode-pesan' name='input-pesan' readOnly={true} />
                                 </div>
                             </div>
                             <div className='d-flex'>
                                 <div className={`${global.input_group} col-12 px-2`}>
-                                    <p className={global.title}>Nama Pesanan <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Nama Pesanan</p>
                                     <input type="text" id='input-nama-pesanan' name='input-nama-pesanan' readOnly={true}/>
                                 </div>
                             </div>
                             <div className='d-flex'>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>Kode Customer <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Kode Customer</p>
                                     <input type="text" id='input-kode-customer' name='input-kode-customer' readOnly={true}/>
                                 </div>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>Nama Customer <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Nama Customer</p>
                                     <input type="text" id='input-nama-customer' name='input-nama-customer' readOnly={true}/>
                                 </div>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>Jumlah <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Jumlah</p>
                                     <input type="text" id='input-jumlah' name='input-jumlah' readOnly={true}/>
                                 </div>
                             </div>
                             <div className='d-flex'>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>HPP <span className={global.important}>*</span></p>
+                                    <p className={global.title}>HPP</p>
                                     <input type="text" id='input-hpp' name='input-hpp' readOnly={true}/>
                                 </div>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>Profit <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Profit</p>
                                     <input type="text" id='input-profit' name='input-profit' readOnly={true}/>
                                 </div>
                                 <div className={`${global.input_group} col-4 px-2`}>
-                                    <p className={global.title}>Harga Jual <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Harga Jual</p>
                                     <input type="text" id='input-harga-jual' name='input-harga-jual' readOnly={true}/>
                                 </div>
                             </div>
                             <div className='d-flex'>
                                 <div className={`${global.input_group} col-12 px-2`}>
-                                    <p className={global.title}>Catatan Pesanan <span className={global.important}>*</span></p>
+                                    <p className={global.title}>Catatan Pesanan</p>
                                     <input type="text" id='input-catatan-pesanan' name='input-catatan-pesanan' readOnly={true}/>
                                 </div>
                             </div>
