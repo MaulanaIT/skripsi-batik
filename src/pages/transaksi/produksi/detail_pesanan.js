e2c7f20bb8a9e63555555e33d21fc2200fba05f8
MaulanaIT <maulana@ideal.id>
2022-08-22 11:41:55 +0700

    fix bug

--- src/pages/transaksi/produksi/detail_pesanan.js
+++ src/pages/transaksi/produksi/detail_pesanan.js
@@ -36,51 +36,51 @@ export default function Detail_pesanan({ kode }) {
                     <div className='d-flex'>
                         <div className={`${global.input_group} col-6 px-2`}>
                             <p className={global.title}>Tanggal</p>
-                            <input type="text" id='input-tanggal-pesan' name='input-tanggal-pesan' value={getDataDetailPesanan?.tanggal} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.tanggal} readOnly={true} />
                         </div>
                         <div className={`${global.input_group} col-6 pe-2`}>
                             <p className={global.title}>Kode Pesanan</p>
-                            <input type="text" id='input-kode-pesan' name='input-pesan' value={getDataDetailPesanan?.tanggal} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.tanggal} readOnly={true} />
                         </div>
                     </div>
                     <div className='d-flex'>
                         <div className={`${global.input_group} col-12 px-2`}>
                             <p className={global.title}>Nama Pesanan</p>
-                            <input type="text" id='input-nama-pesanan' name='input-nama-pesanan' value={getDataDetailPesanan?.nama} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.nama} readOnly={true} />
                         </div>
                     </div>
                     <div className='d-flex'>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>Kode Customer</p>
-                            <input type="text" id='input-kode-customer' name='input-kode-customer' value={getDataDetailPesanan?.kode_customer} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.kode_customer} readOnly={true} />
                         </div>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>Nama Customer</p>
-                            <input type="text" id='input-nama-customer' name='input-nama-customer' value={getDataDetailPesanan?.nama_customer} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.nama_customer} readOnly={true} />
                         </div>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>Jumlah</p>
-                            <input type="text" id='input-jumlah' name='input-jumlah' value={SetNumberFormat(getDataDetailPesanan?.jumlah)} readOnly={true} />
+                            <input type="text" value={SetNumberFormat(getDataDetailPesanan?.jumlah)} readOnly={true} />
                         </div>
                     </div>
                     <div className='d-flex'>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>HPP</p>
-                            <input type="text" id='input-hpp' name='input-hpp' value={SetPriceFormat(getDataDetailPesanan?.hpp)} readOnly={true} />
+                            <input type="text" value={SetPriceFormat(getDataDetailPesanan?.hpp)} readOnly={true} />
                         </div>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>Profit</p>
-                            <input type="text" id='input-profit' name='input-profit' value={`${SetNumberFormat(getDataDetailPesanan?.profit)}%`} readOnly={true} />
+                            <input type="text" value={`${SetNumberFormat(getDataDetailPesanan?.profit)}%`} readOnly={true} />
                         </div>
                         <div className={`${global.input_group} col-4 px-2`}>
                             <p className={global.title}>Harga Jual</p>
-                            <input type="text" id='input-harga-jual' name='input-harga-jual' value={SetPriceFormat(getDataDetailPesanan?.harga_jual)} readOnly={true} />
+                            <input type="text" value={SetPriceFormat(getDataDetailPesanan?.harga_jual)} readOnly={true} />
                         </div>
                     </div>
                     <div className='d-flex'>
                         <div className={`${global.input_group} col-12 px-2`}>
                             <p className={global.title}>Catatan Pesanan</p>
-                            <input type="text" id='input-catatan-pesanan' name='input-catatan-pesanan' value={getDataDetailPesanan?.deskripsi} readOnly={true} />
+                            <input type="text" value={getDataDetailPesanan?.deskripsi} readOnly={true} />
                         </div>
                     </div>
                 </div>
