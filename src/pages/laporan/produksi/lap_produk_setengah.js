cce26053f23b769161fc356da108364bce27b0f9
MaulanaIT <maulana@ideal.id>
2022-08-30 10:55:25 +0700

    update laporan setengah jadi

--- src/pages/laporan/produksi/lap_produk_setengah.js
+++ src/pages/laporan/produksi/lap_produk_setengah.js
@@ -14,7 +14,7 @@ import { AiFillPrinter } from 'react-icons/ai';
 // Import CSS
 import bootstrap from '../../../css/bootstrap.module.css';
 import global from '../../../css/global.module.css';
-import style from '../../../css/laporan/kas/penerimaan_kas.module.css';
+import style from '../../../css/laporan/laporanproduksi.module.css';
 
 const CustomSelect = {
     control: (provided, state) => ({
@@ -57,67 +57,155 @@ const CustomSelect = {
     })
 }
 
-export class lap_produk_setengah extends Component {
+export default function Lap_produk_setengah() {
+    const [getDataFilterLaporan, setDataFilterLaporan] = useState([]);
+    const [getDataNamaProduk, setDataNamaProduk] = useStateWithCallbackLazy([]);
 
-    state = {
-        tabSelected: 0
+    const [getHTMLTableDaftarLaporan, setHTMLTableDaftarLaporan] = useState([]);
+
+    const [getValueFilterLaporan, setValueFilterLaporan] = useState([]);
+    const [getValueNama, setValueNama] = useState([]);
+    const [getValueTanggalAwal, setValueTanggalAwal] = useState(moment().format('YYYY-MM-DD'));
+    const [getValueTanggalAkhir, setValueTanggalAkhir] = useState(moment().add(1, 'days').format('YYYY-MM-DD'));
+
+    useEffect(() => {
+        getProduk();
+
+        setDataFilterLaporan([
+            { value: 'Tanggal', label: 'Tanggal' },
+            { value: 'Nama', label: 'Nama' }
+        ])
+    }, []);
+
+    const getLaporan = () => {
+        axios.get(`${baseURL}/api/laporan/produksi/bahan-setengah-jadi/select.php`, config).then(response => {
+            let data = response.data.data;
+
+            let htmlTableDaftarLaporan = [];
+
+            if (data && data.length > 0) {
+                data.forEach((item, index) => {
+                    if (getValueFilterLaporan?.value === 'Nama') {
+                        if (item.kode_produk === getValueNama?.value) {
+                            htmlTableDaftarLaporan.push(
+                                <tr key={index}>
+                                    <td>{index + 1}.</td>
+                                    <td>{item.tanggal}</td>
+                                    <td>{item.kode_produk}</td>
+                                    <td>{item.nama_produk}</td>
+                                    <td>{item.jumlah}</td>
+                                    <td>{item.departemen}</td>
+                                    <td>{
+                                        item.departemen === 'Designer' ? 'Cap/Canting' :
+                                            item.departemen === 'Cap/Canting' ? 'Pewarnaan' :
+                                                item.departemen === 'Pewarnaan' ? 'Jahit' :
+                                                    item.departemen === 'Jahit' && 'Packing'
+                                    }</td>
+                                </tr>
+                            );
+                        }
+                    } else if (getValueFilterLaporan?.value === 'Tanggal') {
+                        if (moment(item.tanggal).valueOf() >= moment(getValueTanggalAwal).valueOf() &&
+                            moment(item.tanggal).valueOf() <= moment(getValueTanggalAkhir).valueOf()) {
+                            htmlTableDaftarLaporan.push(
+                                <tr key={index}>
+                                    <td>{index + 1}.</td>
+                                    <td>{item.tanggal}</td>
+                                    <td>{item.kode_produk}</td>
+                                    <td>{item.nama_produk}</td>
+                                    <td>{item.jumlah}</td>
+                                    <td>{item.departemen}</td>
+                                    <td>{
+                                        item.departemen === 'Designer' ? 'Cap/Canting' :
+                                            item.departemen === 'Cap/Canting' ? 'Pewarnaan' :
+                                                item.departemen === 'Pewarnaan' ? 'Jahit' :
+                                                    item.departemen === 'Jahit' && 'Packing'
+                                    }</td>
+                                </tr>
+                            );
+                        }
+                    }
+                });
+            }
+
+            setHTMLTableDaftarLaporan(htmlTableDaftarLaporan);
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
-    componentDidMount() {
-        $('#table-data-bahan-baku').DataTable();
+    const getProduk = () => {
+        axios.get(`${baseURL}/api/master/inventory/produk/select.php`, config).then(response => {
+            let data = response.data.data;
+
+            let dataNamaProduk = [];
+
+            if (data && data.length > 0) {
+                for (const item of data) {
+                    dataNamaProduk.push({
+                        value: item.kode,
+                        label: item.nama
+                    });
+                }
+            }
+
+            axios.get(`${baseURL}/api/transaksi/penjualan/estimasi-pesanan/select.php`, config).then(responsePesanan => {
+                let dataPesanan = responsePesanan.data.data;
+    
+                if (dataPesanan && dataPesanan.length > 0) {
+                    for (const item of dataPesanan) {
+                        dataNamaProduk.push({
+                            value: item.kode,
+                            label: item.nama
+                        });
+                    }
+                }
+    
+                setDataNamaProduk(dataNamaProduk);
+            }).catch(error => {
+                console.log(error);
+            });
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
-    SelectProduksi = (value) => {
-        this.setState({ pilihProduksi: value ? value.value : '' });
-    }
-
-    render() {
-        return (
-            <>
+    return (
+        <React.Fragment>
             <div className={style.header}>
                 <p className={style.title}>Laporan Barang Setengah Jadi</p>
                 <p className={style.pathname}>Laporan / Laporan Produksi / Laporan Barang Setengah Jadi </p>
             </div>
             <div className={style.content}>
                 <div className={global.card}>
-                    <div className={`${global.input_group_row} col-6 `}>
+                    <div className={`${global.input_group_row} col-12`}>
                         <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Produksi Berdasarkan</p>
-                        <Select className="col col-lg-4 col-md-3" isClearable={true} isSearchable={true} options={[
-                            { value: 'Tanggal', label: 'Tanggal' },
-                            { value: 'Nama', label: 'Nama' }
-                        ]} placeholder={'Select Produksi...'} styles={CustomSelect} onChange={(value) => this.SelectProduksi(value)} />
+                        <Select className="col col-lg-4 col-md-3" isClearable={true} isSearchable={true} options={getDataFilterLaporan} placeholder={'Select Laporan...'} value={getValueFilterLaporan} styles={CustomSelect} onChange={(value) => setValueFilterLaporan(value)} />
                     </div>
-                    
-                    {this.state.pilihProduksi === 'Tanggal' ?
-                    <>
-                    <div className={`d-flex`}>
-                        <div className={`${global.input_group_row} col-6`}>
-                            <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Tanggal Awal</p>
-                            <input type="date" id='input-tanggal-awal' name='input-tanggal-awal' className="col col-lg-4 col-md-3" />
+
+                    {getValueFilterLaporan?.value === 'Tanggal' &&
+                        <div className={`d-flex`}>
+                            <div className={`${global.input_group_row} col-6 pe-2`}>
+                                <p className={`${global.title} col-12 col-lg-6 col-md-3 pb-2 pb-md-0`}>Tanggal Awal</p>
+                                <input type="date" className="col col-lg-4 col-md-3" value={getValueTanggalAwal} onChange={e => setValueTanggalAwal(e.target.value)} />
+                            </div>
+                            <div className={`${global.input_group_row} col-6 ps-2`}>
+                                <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Tanggal Akhir</p>
+                                <input type="date" className="col col-lg-4 col-md-3" value={getValueTanggalAkhir} onChange={e => setValueTanggalAkhir(e.target.value)} />
+                            </div>
                         </div>
-                        <div className={`${global.input_group_row} col-6`}>
-                            <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Tanggal Akhir</p>
-                            <input type="date" id='input-tanggal-akhir' name='input-tanggal-akhir'/>
+                    }
+
+                    {getValueFilterLaporan?.value === 'Nama' &&
+                        <div className={`${global.input_group_row}`}>
+                            <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Nama Barang</p>
+                            <Select isClearable={true} isSearchable={true} options={getDataNamaProduk} placeholder={'Pilih Barang...'} styles={CustomSelect} className='col-12 col-lg-4' value={getValueNama} onChange={value => setValueNama(value)} />
                         </div>
-                    </div>
-                    </>
-                    : null}
-                    
-                {this.state.pilihProduksi === 'Nama' ?
-                <>
-                     <div className={`${global.input_group_row} col-6`}>
-                        <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Nama Barang</p>
-                        <Select isClearable={true} isSearchable={true} placeholder={'Pilih Barang...'} styles={CustomSelect} className='col-7' />
-                    </div>
-                </>
-                :
-                <>
-                </>
-                }
+                    }
                     <div className='d-flex flex-column gap-2 pt-2'>
                         <div className='d-flex'>
                             <div className='col-6 pe-2'>
-                                <button type='button' className={`${global.button} w-100`}>Cek Laporan</button>
+                                <button type='button' className={`${global.button} w-100`} onClick={getLaporan}>Cek Laporan</button>
                             </div>
                             <div className='col-6 ps-2'>
                                 <button type='button' className={`${global.button} w-100`} style={{ "--button-first-color": '#8e0000', "--button-second-color": '#a06565' }}>Batal</button>
@@ -125,57 +213,60 @@ export class lap_produk_setengah extends Component {
                         </div>
                     </div>
                 </div>
-                <div className={`${global.card} col-12`}>
-                    <div className='d-flex'>
-                        <div className='col-10'>
-                            <p className={global.title}></p>
-                        </div>
-                        <div className={`${global.cursor_pointer} ms-auto pe-5`}>
-                            <TiExport className='fs-4' />
-                        </div>
-                    </div>
-                    <div className={cx([global.card, global.boxless])}>
-                        <p className={`${style.title} text-center w-100 fw-bold`}>LEKSANA BATIK JAYA</p>
-                        <p className={`${style.title} text-center w-100 fw-bold`}>LAPORAN PERSEDIAAN BARANG SETENGAH JADI</p>
-                        <div className={`d-flex`}>
-                            <div className={`${global.input_group_row} col-6`}>
-                                <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Nama Produk</p>
-                                <input type="text" id='input-nama-produk' name='input-nama-produk' readOnly={true} />
+                {getHTMLTableDaftarLaporan?.length > 0 &&
+                    <div className={`${global.card} col-12`}>
+                        <div className='d-flex'>
+                            <div className='col-10'>
+                                <p className={global.title}></p>
+                            </div>
+                            <div className={`${global.cursor_pointer} ms-auto pe-5`}>
+                                <TiExport className='fs-4' />
                             </div>
                         </div>
-                        <div className={`${bootstrap[`d-flex`]}`}>
-                            <div className={`${global.input_group_row} col-6`}>
-                                <p className={`${global.title} col-6 col-lg-3 col-md-3 pb-2 pb-md-0`}>Tanggal Produksi</p>
-                                <input type="date" id='input-tanggal-awal-produksi' name='input-tanggal-awal-produksi' readOnly={true} />
-                                <p className={`${global.title} col-1 ps-2`}>s/d</p>
-                                <input type="date" id='input-tanggal-akhir-produksi' name='input-tanggal-akhir-produksi' readOnly={true} />
+                        <div className={cx([global.card, global.boxless])}>
+                            <p className={`${style.title} text-center w-100 fw-bold`}>LEKSANA BATIK JAYA</p>
+                            <p className={`${style.title} text-center w-100 fw-bold`}>LAPORAN PERSEDIAAN BARANG SETENGAH JADI</p>
+                            {getValueFilterLaporan?.value === 'Nama' &&
+                                <div className={`d-flex`}>
+                                    <div className={`${global.input_group_row} col-6`}>
+                                        <p className={`${global.title} col-12 col-lg-3 col-md-3 pb-2 pb-md-0`}>Nama Produk</p>
+                                        <input type="text" value={getValueNama?.label} readOnly={true} />
+                                    </div>
+                                </div>
+                            }
+                            {getValueFilterLaporan?.value === 'Tanggal' &&
+                                <div className={`${bootstrap[`d-flex`]} flex-wrap`}>
+                                    <div className={`${global.input_group_row} col-12`}>
+                                        <p className={`${global.title} col-4 col-lg-2 pb-2 pb-md-0`}>Tanggal Produksi</p>
+                                        <input type="date" className='col-4 col-lg-2' value={getValueTanggalAwal} readOnly={true} />
+                                        <p className={`${global.title} col-auto px-2`}>s/d</p>
+                                        <input type="date" className='col-4 col-lg-2' value={getValueTanggalAkhir} readOnly={true} />
+                                    </div>
+                                </div>
+                            }
+                            <br></br>
+                            <div className={`table-responsive`}>
+                                <table id='table-data' className={`table w-100`}>
+                                    <thead className="align-middle text-center text-nowrap">
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Tanggal Produksi</th>
+                                            <th>Kode Barang</th>
+                                            <th>Nama Barang</th>
+                                            <th>Qty</th>
+                                            <th>Dep Asal</th>
+                                            <th>Dep Tujuan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {getHTMLTableDaftarLaporan}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
-                        <br></br>
-                        <div className={`table-responsive`}>
-                            <table id='table-data' className={`table w-100`}>
-                                <thead className="align-middle text-center text-nowrap">
-                                    <tr>
-                                        <th>No.</th>
-                                        <th>Tanggal Produksi</th>
-                                        <th>Kode Barang</th>
-                                        <th>Nama Barang</th>
-                                        <th>Qty</th>
-                                        <th>Dep Asal</th>
-                                        <th>Dep Tujuan</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                </tbody>
-                            </table>
-                        </div>
                     </div>
-                </div>
-                </div>
-            </>
-        )
-    }
+                }
+            </div>
+        </React.Fragment>
+    )
 }
-
-export default lap_produk_setengah
